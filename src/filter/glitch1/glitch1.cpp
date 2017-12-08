e140fadfc9d06a264dbb249f097c8c22309f34bb
vloop <marc.perilleux@laposte.net>
2017-12-08 18:30:47 +0100

    Signed-off-by: vloop <marc.perilleux@laposte.net>

--- src/filter/glitch1/glitch1.cpp
+++ src/filter/glitch1/glitch1.cpp
@@ -34,21 +34,25 @@ union px_t {
 
 class glitch1 : public frei0r::filter {
 private:
-	f0r_param_double delay_x, delay_y, wet, dry;
+	f0r_param_double delay_x, delay_y, wet, dry, mode, rotcol;
 	unsigned int _width;
 	unsigned int _height;
 	
 public:
 	glitch1(unsigned int width, unsigned int height)
 	{
-		delay_x = 1;
-		delay_y = 1;
+		delay_x = .5;
+		delay_y = .5;
 		wet = .5;
 		dry = .5;
+		mode = 0;
+		rotcol = 0;
 		register_param(delay_x, "Delay X", "Delay X");
 		register_param(delay_y, "Delay Y", "Delay Y");
 		register_param(wet, "wet", "wet");
 		register_param(dry, "dry", "dry");
+		register_param(mode, "mode", "mode");
+		register_param(rotcol, "rot.col.", "rot.col.");
 		_width = width;
 		_height = height;
 	}
@@ -59,41 +63,101 @@ public:
 	                    uint32_t* out,
                         const uint32_t* in) {
 		unsigned char c;
-		unsigned long delay;
-		// delay = (unsigned long)((f0r_param_double)size * factor);
-		delay = long(delay_x * double(_width))+long(delay_y * double(_height)) * _width;
-		for (int i = 0; i < delay; i++) {
-		  out[i]=in[i];
+		long delay;
+		delay = 2 * ((delay_x - 0.5) + (delay_y - 0.5) * double(_height)) * double(_width);
+		
+		if (delay>0){
+		  for (int i = 0; i < delay; i++) {
+		    out[i]=in[i];
+		  }
+		} {
+		  for (int i = size - 1; i > size - 1 + delay; i--) {
+		    out[i]=in[i];
+		  }
 		}
-		for (int i = delay; i < size; i++) {
-			px_t pi, pi2;
-			pi.u = out[i-delay];
-			pi2.u = in[i];
-			/* Color rotate
-			c=pi.c[0];
-			pi.c[0] = pi.c[1];
-			pi.c[1] = pi.c[2];
-			pi.c[2] = c;
-			*/
-			/* XOR mode
-			pi.c[0]=pi.c[0] ^ pi2.c[0];
-			pi.c[1]=pi.c[1] ^ pi2.c[1];
-			pi.c[2]=pi.c[2] ^ pi2.c[2];
-			*/
-			/* 4-channel = 1 channel per video component 
-			pi.c[0]=std::min(float(pi.c[0])*wet + float(pi2.c[0])*dry,255.0);
-			pi.c[1]=std::min(float(pi.c[1])*wet + float(pi2.c[1])*dry,255.0);
-			pi.c[2]=std::min(float(pi.c[2])*wet + float(pi2.c[2])*dry,255.0);
-			pi.c[3]=std::min(float(pi.c[3])*wet + float(pi2.c[3])*dry,255.0);
-                        */
-			// Handle as mono sound
-			pi.u = pi.u * wet + pi2.u * dry;
-			/* Handle as stereo sound
-			pi.s[0]=pi.s[0]+pi2.s[0];
-			pi.s[1]=pi.s[1]+pi2.s[1];
-			*/
-			out[i] = pi.u;
-		}
+		
+		
+		for (int i = abs(delay); i < size; i++) {
+			px_t p_src, p_dest;
+			if (delay > 0){
+			  p_src.u = out[i-delay];
+			  p_dest.u = in[i];
+			}else{
+			  p_src.u = out[size - 1 - i + delay];
+			  p_dest.u = in[size - 1 - i];
+			}
+		
+			// Color rotate
+			switch(int(rotcol*6)){
+			  case 0: // RGB
+			    break;
+			  case 1: // RBG
+			    c=p_src.c[1];
+			    p_src.c[1] = p_src.c[2];
+			    p_src.c[2] = c;
+			    break;
+			  case 2: // GRB
+			    c=p_src.c[1];
+			    p_src.c[1] = p_src.c[0];
+			    p_src.c[0] = c;
+			    break;
+			  case 3: // GBR
+			    c=p_src.c[0];
+			    p_src.c[0] = p_src.c[1];
+			    p_src.c[1] = p_src.c[2];
+			    p_src.c[2] = c;
+			    break;
+			  case 4: // BGR
+			    c=p_src.c[2];
+			    p_src.c[2] = p_src.c[0];
+			    p_src.c[0] = c;
+			    break;
+			  default: // BRG
+			    c=p_src.c[2];
+			    p_src.c[2] = p_src.c[1];
+			    p_src.c[1] = p_src.c[0];
+			    p_src.c[0] = c;
+			    break;
+			}
+			switch(int(mode*5)){
+			  case 0:
+			      // 4-channel = 1 channel per video component, bounded
+			      p_src.c[0]=std::min(float(p_src.c[0])*wet + float(p_dest.c[0])*dry,255.0);
+			      p_src.c[1]=std::min(float(p_src.c[1])*wet + float(p_dest.c[1])*dry,255.0);
+			      p_src.c[2]=std::min(float(p_src.c[2])*wet + float(p_dest.c[2])*dry,255.0);
+			      p_src.c[3]=std::min(float(p_src.c[3])*wet + float(p_dest.c[3])*dry,255.0);
+			      break;
+			  case 1:
+			      // 4-channel = 1 channel per video component, unbounded
+			      p_src.c[0]=float(p_src.c[0])*wet + float(p_dest.c[0])*dry;
+			      p_src.c[1]=float(p_src.c[1])*wet + float(p_dest.c[1])*dry;
+			      p_src.c[2]=float(p_src.c[2])*wet + float(p_dest.c[2])*dry;
+			      p_src.c[3]=float(p_src.c[3])*wet + float(p_dest.c[3])*dry;
+			      break;
+			  case 2:
+			      // XOR mode
+			      p_src.c[0]=int(p_src.c[0] * wet) ^ int(p_dest.c[0] * dry);
+			      p_src.c[1]=int(p_src.c[1] * wet) ^ int(p_dest.c[1] * dry);
+			      p_src.c[2]=int(p_src.c[2] * wet) ^ int(p_dest.c[2] * dry);
+			      p_src.c[3]=int(p_src.c[2] * wet) ^ int(p_dest.c[3] * dry);
+			      break;
+			  case 3:
+			      // Handle as mono sound
+			      p_src.u = p_src.u * wet + p_dest.u * dry;
+			      break;
+			  default:
+			      // Handle as stereo sound
+			      p_src.s[0]=p_src.s[0] * wet +p_dest.s[0] * dry;
+			      p_src.s[1]=p_src.s[1] * wet +p_dest.s[1] * dry;
+			      break;
+			}
+			if (delay > 0){
+			  out[i] = p_src.u;
+			}else{
+			  out[size - 1 - i] = p_src.u;
+			}
+		  }
+		  
 	}
 };
 
